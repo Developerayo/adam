e7e44af2796b294f176757d6b04d10fdb326dc5b
developerayo <shodipovi@gmail.com>
2024-09-04 21:03:49 +0100

    this was fun

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,15 @@
 import os from 'os'
 import path from 'path'
-import fs from 'fs/promises'
+import fs from 'fs'
 
 export function getOsType() {
   return os.type()
 }
 
-export async function checkForFileOrFolder(name, isFile = false) {
+export function checkForFileOrFolder(name, isFile = false) {
   const fullPath = path.join(process.cwd(), name)
   try {
-    const stats = await fs.stat(fullPath)
+    const stats = fs.statSync(fullPath)
     return isFile ? stats.isFile() : stats.isDirectory()
   } catch (error) {
     return false
